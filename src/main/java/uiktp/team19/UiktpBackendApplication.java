6c8cb2a9edcd9ab1e08d0cdf9147a90fefdd2ae1
David Stojkov <davidstojkov23@gmail.com>
2026-03-24 10:41:03 +0100

    initial commit

---
+++ src/main/java/uiktp/team19/UiktpBackendApplication.java
@@ -0,0 +1,13 @@
+package uiktp.team19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiktpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiktpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uiktp/team19/UiktpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uiktp.team19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiktpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
