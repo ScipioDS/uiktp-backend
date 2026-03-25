9e8a7b036ce468c6ab3857736467b87d7d668bea
David Stojkov <davidstojkov23@gmail.com>
2026-03-25 14:27:47 +0100

    KAN-DS - initial weather api set up

---
+++ src/main/java/uiktp/team19/model/auth/User.java
@@ -0,0 +1,28 @@
+package uiktp.team19.model.auth;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String email;
+
+    private String username;
+
+    private String password;
+}
